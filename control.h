6bd5887bf4770e429c58f68ff9a8083ac916c99d
Yun Hye Nam <yunnam0924@gmail.com>
2024-12-07 16:10:55 -0500

    control class updates
    control class fixes (no authentication, added allUsers)
    
    fixes

--- control.h
+++ control.h
@@ -17,19 +17,22 @@ class Control {
         void addUser(const User& user);
         void deleteUser(QString email);
         void updateUser(QString email, const User& user);
-        bool login(QString email, QString password);
+        // bool login(QString email, QString password);
+        bool login(QString email);
         bool createAccount(QString email, QString password, QString name, int age, QString gender, float height, float weight);
 
         HealthData* processData(const RawHealthData& rawHealthData);
         void displayHistoricalData(const QVector<HealthData>& historicalData);
 
         bool connectToHardware(Hardware* hardware);
-        // disconnect?
+        // bool disconnectFromHardware(Hardware* hardware);
+
         bool createNewScan();
         bool createCharts();
     private:
-        QVector<Hardware*> connectedHardware;
-        QVector<User*> currentUser;
+        Hardware* connectedHardware;
+        QVector<User*> allUsers;
+        User* currentUser;
 
 }
 
